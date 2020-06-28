bfa01c4ea9482cd91c282e3f693245c772fbc771
Suraj kc <isurajkc@gmail.com>
2020-06-28 19:51:16 +0545

    Customize storybook with brand title and brand url

---
+++ .storybook/theme.js
@@ -0,0 +1,7 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "light",
+  brandTitle: "TS Snippets",
+  brandUrl: "https://github.com/kcsuraj/typescript-snippets",
+});
